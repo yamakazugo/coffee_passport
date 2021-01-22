59ea3c6cdb6f8c0dcfe51976b79b13130982f310
yamakazugo <yamakazugo@me.com>
2021-01-22 15:10:40 +0900

    いいね！機能の実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes
+  has_many :liked_users, through: :likes, source: :user
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts, dependent: :destroy
   has_many :comments
-  
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
+
+  def already_liked?(post)
+    self.likes.exists?(post_id: post.id)
+  end
+
+
   validates :nickname, presence: true
 
 end
