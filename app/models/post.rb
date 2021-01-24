868dacfb405e45e4025e5d097398edaae87d92d8
yamakazugo <yamakazugo@me.com>
2021-01-24 15:44:23 +0900

    コードの修正

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
-  has_many :likes
+  has_many :likes, dependent: :destroy
   has_many :liked_users, through: :likes, source: :user
   has_one_attached :image
 
